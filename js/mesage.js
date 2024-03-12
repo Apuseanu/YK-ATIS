624b3dff94088a2e34763b1aa151d2e96b05e1d4
Apuseanu <bogdanmaidanskii@yahoo.com>
2024-03-12 23:09:02 +0200

    update_FAQ

--- js/mesage.js
+++ js/mesage.js
@@ -1,47 +1,20 @@
-document.getElementById("send-button").addEventListener("click", function() {
-    const textarea = document.getElementById("textarea").value;
-    const email = document.getElementById("email").value;
-    const errorMsg = document.getElementById("error-msg");
-
-    if (textarea.trim() === "" || email.trim() === "") {
-        errorMsg.textContent = "Te rugăm să completezi ambele câmpuri.";
-        return;
-    }
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-        errorMsg.textContent = "Te rugăm să introduci o adresă de email validă.";
-        return;
-    }
-
-    errorMsg.textContent = "";
-
-    const nodemailer = require('nodemailer');
-
-// Configurare transporter pentru a trimite e-mailuri folosind serviciul SMTP
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'adresa.email@gmail.com', // Adresa de e-mail a expeditorului
-        pass: 'parola' // Parola pentru adresa de e-mail a expeditorului
-    }
-});
-
-// Definirea opțiunilor pentru e-mailul de trimis
-const mailOptions = {
-    from: 'adresa.email@gmail.com', // Adresa de e-mail a expeditorului
-    to: 'bogdanmaidanskii@gmail.com', // Adresa de e-mail a destinatarului
-    subject: 'Subiectul e-mailului', 
-    text: 'Aici este conținutul e-mailului.' // Conținutul e-mailului
-};
-
-
-transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-        console.log('Eroare la trimiterea e-mailului:', error);
-    } else {
-        console.log('E-mail trimis cu succes:', info.response);
-    }
-});
-
-});
+var formm = document.getElementsByTagName('form')[0];
+           formm.addEventListener('submit',contact,false);
+           function contact(e) {
+              e.preventDefault();
+        
+              var target = e.target || e.srcElement;
+              var i = 0;
+              var message = '';
+        
+              for(i = 0; i < target.length; ++i) {
+                 if(target[i].type != 'text' && target[i].type != 'textarea') {
+                    continue;
+                 }
+        
+                 message += target[i].name + ': ' + target[i].value + "\r\n";
+              }
+              target.elements["body"].value = message;
+        
+              this.submit();
+           }
